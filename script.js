6fb1300d7cdcfdfaa5475dea15722b6c4f70008b
Ashutosh8215 <ashutosh.j@mindfiresolutions.com>
2022-10-20 13:36:39 +0530

    Eraser button

--- script.js
+++ script.js
@@ -4,7 +4,6 @@
 
 const canvas = document.getElementById("drawing-board");
 const toolbar = document.getElementById("toolbar");
-
 const ctx = canvas.getContext("2d");
 
 const canvasOffsetX = canvas.offsetLeft;
@@ -15,9 +14,11 @@ canvas.height = window.innerHeight - canvasOffsetY;
 
 let isPainting = false;
 let paintBtn = false;
+let eraserBtn = false;
 let lineWidth = 5;
 let startX;
 let startY;
+let tool;
 
 toolbar.addEventListener("change", (event) => {
   switch (event.target.id) {
@@ -30,24 +31,64 @@ toolbar.addEventListener("change", (event) => {
   }
 });
 
-canvas.addEventListener("mousedown", (event) => {
-  isPainting = true;
-  startX = event.x;
-  startY = event.y;
-});
+canvas.onmousedown = (event) => {
+  switch (tool) {
+    case "Pencil":
+      isPainting = true;
+      startX = event.x;
+      startY = event.y;
+      break;
+    case "Eraser":
+      break;
+    default:
+      break;
+  }
+};
 
 canvas.addEventListener("mouseup", (event) => {
-  isPainting = false;
-  ctx.stroke();
-  ctx.beginPath();
+  switch (tool) {
+    case "Pencil":
+      isPainting = false;
+      ctx.stroke();
+      ctx.beginPath();
+      break;
+    case "Eraser":
+      break;
+    default:
+      break;
+  }
 });
 
 canvas.addEventListener("mousemove", (event) => {
-  if (isPainting && paintBtn) {
-    ctx.lineWidth = lineWidth;
-    ctx.lineCap = "round";
+  switch (tool) {
+    case "Pencil":
+      if (isPainting && paintBtn) {
+        ctx.lineWidth = lineWidth;
+        ctx.lineCap = "round";
+        ctx.lineTo(event.x - canvasOffsetX, event.y);
+        ctx.stroke();
+      }
+      break;
+    case "Eraser":
+      ctx.fillStyle = "white";
+      ctx.lineWidth = 2;
+      ctx.beginPath();
+      let eraserX = event.x - canvasOffsetX;
+      let eraserY = event.y - canvasOffsetY;
+      ctx.rect(eraserX, eraserY, 10, 10);
+      ctx.fill();
 
-    ctx.lineTo(event.x - canvasOffsetX, event.y);
-    ctx.stroke();
+      break;
+    default:
+      break;
   }
 });
+
+function pencil() {
+  paintBtn = !paintBtn;
+  tool = paintBtn ? "Pencil" : undefined;
+}
+function eraser() {
+  eraserBtn = !eraserBtn;
+  tool = eraserBtn ? "Eraser" : undefined;
+}
