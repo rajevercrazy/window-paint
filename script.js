a76a76f6441f51c40c35e1497de58820dbddfb34
Ashutosh8215 <ashutosh.j@mindfiresolutions.com>
2022-10-26 16:29:48 +0530

    Add shapes feature and Add Color filler feature

--- script.js
+++ script.js
@@ -1,207 +1,269 @@
 /**
  * @type HTMLCanvasElement
  */
-const toolbar = document.getElementById("toolbar");
 
-const canvas = document.getElementById("drawing-board");
-const ctx = canvas.getContext("2d");
+ class Shapes {
+  x;
+  y;
+  ctx;
 
-// canvas.width = 1152;
-// canvas.height = 648;
-
-const canvasOffsetX =  canvas.offsetLeft;
-const canvasOffsetY =  canvas.offsetTop;
-
-// const topCanvas = document.getElementById("top-drawing-board");
-// const topCtx = topCanvas.getContext("2d");
-
-// const topCanvasOffsetX = topCanvas.offsetLeft;
-// const topCanvasOffsetY = topCanvas.offsetTop;
-
-// topCanvas.width = window.innerWidth - topCanvasOffsetX;
-// topCanvas.height = window.innerHeight - topCanvasOffsetY;
-
-let isPainting = false;
-let isEraser = false;
-let paintBtn = false;
-let eraserBtn = false;
-let textBtn = false;
-let lineBtn = false;
-let curveLineBtn = false;
-let lineWidth = 1;
-let startX;
-let startY;
-let tool;
-
-toolbar.addEventListener("change", (event) => {
-  switch (event.target.id) {
-    case "stroke":
-      ctx.strokeStyle = event.target.value;
-      break;
-    // case "lineWidth":
-    //   lineWidth = event.target.value;
-    //   break;
+  constructor(ctx) {
+    this.ctx = ctx;
   }
-});
 
-canvas.onmousedown = (event) => {
-  switch (tool) {
-    case "Pencil":
+  rectangle() {
+    this.ctx.beginPath();
+    this.ctx.rect(this.x, this.y, 10, 25);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
 
-      isPainting = true;
+  circle() {
+    this.ctx.beginPath()
+    this.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
+
+  triangle() {
+    this.ctx.beginPath()
+    this.ctx.moveTo(this.x, this.y);
+    this.ctx.lineTo(this.x - 20, this.y + 20);
+    this.ctx.lineTo(this.x + 20 ,this.y + 20);
+    this.ctx.lineTo(this.x, this.y);
+    this.ctx.stroke();
+    this.ctx.closePath();
+  }
+
+  draw(shape){
+    switch(shape){
+      case 'circle': 
+      this.circle();
       break;
-    case "Eraser":
-      isEraser = true;
+      case 'rectangle': 
+      this.rectangle();
       break;
-    case "Text":
-      if (textBtn) {
-        addInput(event.x, event.y);
-      }
+      case 'triangle': 
+      this.triangle();
       break;
-    case "Line":
-      if (lineBtn) {
+      default:
+      break;
+    }
+  }
+}
+
+  const toolbar = document.getElementById("toolbar");
+  const canvas = document.getElementById("drawing-board");
+  const ctx = canvas.getContext("2d");
+  const shapes = new Shapes(ctx);
+  // canvas.width = 1152;
+  // canvas.height = 648;
+
+  const canvasOffsetX = canvas.offsetLeft;
+  const canvasOffsetY = canvas.offsetTop;
+
+  // const topCanvas = document.getElementById("top-drawing-board");
+  // const topCtx = topCanvas.getContext("2d");
+
+  // const topCanvasOffsetX = topCanvas.offsetLeft;
+  // const topCanvasOffsetY = topCanvas.offsetTop;
+
+  // topCanvas.width = window.innerWidth - topCanvasOffsetX;
+  // topCanvas.height = window.innerHeight - topCanvasOffsetY;
+
+  let isPainting = false;
+  let isEraser = false;
+  let paintBtn = false;
+  let eraserBtn = false;
+  let textBtn = false;
+  let lineBtn = false;
+  let curveLineBtn = false;
+  let lineWidth = 1;
+  let tool;
+  toolbar.addEventListener("change", (event) => {
+    switch (event.target.id) {
+      case "stroke":
+        ctx.strokeStyle = event.target.value;
+        setColor(event.target.value);
+        break;
+      // case "lineWidth":
+      //   lineWidth = event.target.value;
+      //   break;
+    }
+  });
+
+  canvas.onmousedown = (event) => {
+    switch (tool) {
+      case "Pencil":
+        isPainting = true;
+        break;
+      case "Eraser":
+        isEraser = true;
+        break;
+      case "Text":
+        if (textBtn) {
+          addInput(event.x, event.y);
+        }
+        break;
+      case "Shape":
+        drawShape( event.clientX - canvasOffsetX,event.clientY - canvasOffsetY);
+        break;
+      default:
+        break;
+    }
+  };
+
+  canvas.addEventListener("mouseup", (event) => {
+    switch (tool) {
+      case "Pencil":
+        isPainting = false;
+        ctx.stroke();
         ctx.beginPath();
-        ctx.moveTo(event.x - canvasOffsetX, event.y - canvasOffsetY);
-      }
-      break;
-    default:
-      break;
-  }
-};
-
-canvas.addEventListener("mouseup", (event) => {
-  switch (tool) {
-    case "Pencil":
-      isPainting = false;
-      ctx.stroke();
-      ctx.beginPath();
-      break;
-    case "Eraser":
-      isEraser = false;
-      ctx.stroke();
-      ctx.beginPath();
-      break;
-    case "Line":
-      if (lineBtn) {
-        ctx.lineTo(event.x - canvasOffsetX, event.y - canvasOffsetY);
-        ctx.strokeStyle = 'black';
+        break;
+      case "Eraser":
+        isEraser = false;
         ctx.stroke();
-      }
-      break;
-    default:
-      break;
-  }
-});
+        ctx.beginPath();
+        break;
+      case "Line":
+        if (lineBtn) {
+          ctx.lineTo(event.x - canvasOffsetX, event.y - canvasOffsetY);
+          ctx.strokeStyle = "black";
+          ctx.stroke();
+        }
+        break;
+      default:
+        break;
+    }
+  });
 
-canvas.addEventListener("mousemove", (event) => {
-  switch (tool) {
-    case "Pencil":
-      
-      if (isPainting && paintBtn) {
-        ctx.lineWidth = lineWidth;
-        ctx.lineCap = "round";
-        ctx.lineTo(event.clientX - canvasOffsetX,event.clientY - canvasOffsetY);
-        ctx.stroke();
-      }
-      break;
-    case "Eraser":
-      if (isEraser && eraserBtn) {
-        ctx.strokeStyle = "white";
-        ctx.lineWidth = lineWidth;
-        ctx.lineCap = "square";
-        ctx.lineTo(event.x - canvasOffsetX, event.y);
-        ctx.stroke();
-      }
-      break;
-    default:
-      break;
-  }
-});
+  canvas.addEventListener("mousemove", (event) => {
+    switch (tool) {
+      case "Pencil":
+        if (isPainting && paintBtn) {
+          ctx.lineWidth = lineWidth;
+          ctx.lineCap = "round";
+          ctx.lineTo(
+            event.clientX - canvasOffsetX,
+            event.clientY - canvasOffsetY
+          );
+          ctx.stroke();
+        }
+        break;
+      case "Eraser":
+        if (isEraser && eraserBtn) {
+          ctx.strokeStyle = "white";
+          ctx.lineWidth = lineWidth;
+          ctx.lineCap = "square";
+          ctx.lineTo(event.x - canvasOffsetX, event.y);
+          ctx.stroke();
+        }
+        break;
+      default:
+        break;
+    }
+  });
 
-function pencil() {
-  paintBtn = !paintBtn;
-  tool = paintBtn ? "Pencil" : undefined;
-  if (paintBtn) {
-    eraserBtn = false;
+  function pencil() {
+    paintBtn = !paintBtn;
+    tool = paintBtn ? "Pencil" : undefined;
+    if (paintBtn) {
+      eraserBtn = false;
+      textBtn = false;
+      lineBtn = false;
+    }
+  }
+
+  function eraser() {
+    eraserBtn = !eraserBtn;
+    tool = eraserBtn ? "Eraser" : undefined;
+
+    if (eraserBtn) {
+      paintBtn = false;
+      textBtn = false;
+      lineBtn = false;
+      // <div id="eraseCursor" class="erase-square"></div>
+      // cursor = document.createElement("div");
+      // cursor.setAttribute(class,"erase-square");
+      // console.log(cursor);
+      // canvas.style.cursor = "crosshair";
+    }
+
+    // if (!eraserBtn) {
+    //   canvas.style.removeProperty("cursor");
+    // }
+  }
+
+  function text() {
+    textBtn = !textBtn;
+    tool = textBtn ? "Text" : undefined;
+
+    if (textBtn) {
+      eraserBtn = false;
+      paintBtn = false;
+      lineBtn = false;
+    }
+  }
+
+  function drawShape(x,y) {
+      eraserBtn = false;
+      paintBtn = false;
+      textBtn = false;
+
+      shapes.x = x;
+      shapes.y = y;
+      shapes.draw(shape)
+  }
+
+  function curveLine() {
+    curveLineBtn = !curveLine;
+    if (curveLineBtn) {
+      eraserBtn = false;
+      paintBtn = false;
+      textBtn = false;
+      lineBtn = false;
+    }
+  }
+
+  function addInput(x, y) {
+    let textarea = document.createElement("textarea");
+    textarea.style.position = "fixed";
+    textarea.style.left = x - 4 + "px";
+    textarea.style.top = y - 4 + "px";
+    textarea.onkeydown = handleEnter;
+    document.body.appendChild(textarea);
     textBtn = false;
-    lineBtn = false;
   }
+
+  function handleEnter(event) {
+    let keyCode = event.keyCode;
+    if (keyCode == 13) {
+      ctx.textBaseline = "top";
+      ctx.textAlign = "left";
+      ctx.font = "14px sans-serif";
+      ctx.fillText(
+        this.value,
+        parseInt(this.style.left, 10) - 4,
+        parseInt(this.style.top, 10) - 4
+      );
+      textBtn = true;
+      document.body.removeChild(this);
+    }
+  }
+
+
+function setShape(s){
+  tool = "Shape";
+  shape = s;
 }
 
-function eraser() {
-  eraserBtn = !eraserBtn;
-  tool = eraserBtn ? "Eraser" : undefined;
-
-  if (eraserBtn) {
-    paintBtn = false;
-    textBtn = false;
-    lineBtn = false;
-    // <div id="eraseCursor" class="erase-square"></div>
-    // cursor = document.createElement("div");
-    // cursor.setAttribute(class,"erase-square");
-    // console.log(cursor);
-    canvas.style.cursor = 'crosshair';
-  }
-
-  if(!eraserBtn){
-    canvas.style.removeProperty("cursor");
-  }
+function rotate(degree) {
+  ctx.rotate(degree * Math.PI / 2);
 }
 
-function text() {
-  textBtn = !textBtn;
-  tool = textBtn ? "Text" : undefined;
-
-  if (textBtn) {
-    eraserBtn = false;
-    paintBtn = false;
-    lineBtn = false;
-  }
-}
-
-function drawLine() {
-  lineBtn = !lineBtn;
-  tool = lineBtn ? "Line" : undefined;
-
-  if (lineBtn) {
-    eraserBtn = false;
-    paintBtn = false;
-    textBtn = false;
-  }
-}
-
-function curveLine() {
-  curveLineBtn =  !curveLine;
-  if (curveLineBtn) {
-    eraserBtn = false;
-    paintBtn = false;
-    textBtn = false;
-    lineBtn = false;
-  }
-}
-
-function addInput(x, y) {
-  let textarea = document.createElement("textarea");
-  textarea.style.position = "fixed";
-  textarea.style.left = x - 4 + "px";
-  textarea.style.top = y - 4 + "px";
-  textarea.onkeydown = handleEnter;
-  document.body.appendChild(textarea);
-  textBtn = false;
-}
-
-function handleEnter(event) {
-  let keyCode = event.keyCode;
-  if (keyCode == 13) {
-    ctx.textBaseline = "top";
-    ctx.textAlign = "left";
-    ctx.font = "14px sans-serif";
-    ctx.fillText(
-      this.value,
-      parseInt(this.style.left, 10) - 4,
-      parseInt(this.style.top, 10) - 4
-    );
-    textBtn = true;
-    document.body.removeChild(this);
-  }
+let colorId='color1';
+function setColor(color){
+  let selectedColor = document.getElementById(colorId);
+  selectedColor.style.backgroundColor = color;
+  ctx.strokeStyle = color;
 }
