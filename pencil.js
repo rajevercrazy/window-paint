6de83b3c34679cd93926b178f7ba4f083d48cb17
Ashutosh8215 <ashutosh.j@mindfiresolutions.com>
2022-11-01 09:38:30 +0530

    Update pencil.js
    Change variable name size to lineWidth

--- pencil.js
+++ pencil.js
@@ -1,13 +1,13 @@
-function Pencil(size,strokeStyle,isDrawing) {
+function Pencil(lineWidth,strokeStyle,isDrawing) {
   this.obj= "Pencil"
   this.isDrawing = isDrawing;
-  this.size = size;
+  this.lineWidth = lineWidth;
   this.arr = [];
   this.strokeStyle = strokeStyle;
   this.lineCap = 'round';
 
   this.addPointer = (x, y, lineWidth, strokeStyle) => {
-    this.size = lineWidth;
+    this.lineWidth = lineWidth;
     this.strokeStyle = strokeStyle;
     this.arr.push([x, y]);
   }
