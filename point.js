a4e473be169b39e39bd259bdeaf29e6c99a19db6
Ashutosh Jena <ashutosh.j@mindfiresolutions.com>
2022-11-08 12:48:02 +0530

    Improve the point rotation calculation

--- point.js
+++ point.js
@@ -3,13 +3,13 @@ function Point(x,y) {
     this.yCoordinate = y;
 }
 
-Point.prototype.rotatedAPoint = function(origin,angle){
+Point.prototype.rotate = function(origin,angle){
 
     let tetha = angle * Math.PI / 180;
 
     let dx = origin.xCoordinate - this.xCoordinate;
     let dy = origin.yCoordinate - this.yCoordinate;
 
-    this.xCoordinate = Math.cos(tetha) * (dx) - Math.sin(tetha) * (dy) + this.xCoordinate,
-    this.yCoordinate = Math.sin(tetha) * (dx) + Math.cos(tetha) * (dy) + this.yCoordinate
+    this.xCoordinate = Math.cos(tetha) * (dx) - Math.sin(tetha) * (dy) + origin.xCoordinate;
+    this.yCoordinate = Math.sin(tetha) * (dx) + Math.cos(tetha) * (dy) + origin.yCoordinate;
 }
