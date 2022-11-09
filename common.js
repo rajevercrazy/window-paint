2950cfbfb58790116fcefde86cbc1c71e837b69c
Ashutosh Jena <ashutosh.j@mindfiresolutions.com>
2022-11-09 16:15:16 +0530

    code cleaning

--- common.js
+++ common.js
@@ -12,30 +12,30 @@ const commonModules = (() => {
 
   draw = () => {
 
-    for (let shape of shapeLis) {
+    shapeLis.forEach(shape => {
       if (shape.name == "Pencil" || shape.name == "Eraser") {
         drawLine(shape)
-      } 
+      }
       else if (shape.name == "Text") {
         shape.draw();
-      } 
+      }
       else {
         shape.draw();
       }
-    }
+    });
   };
 
   drawLine = (shape) => {
     ctx.beginPath();
-        ctx.moveTo(shape.arr[0].xCoordinate, shape.arr[0].yCoordinate);
-        ctx.lineWidth = shape.lineWidth;
-        ctx.strokeStyle = shape.strokeStyle;
-        ctx.lineCap = shape.lineCap;
-        shape.arr.forEach(element => {
-          ctx.lineTo(element.xCoordinate, element.yCoordinate);
-          ctx.stroke(); 
-        });
-        ctx.closePath();
+    ctx.moveTo(shape.arr[0].xCoordinate, shape.arr[0].yCoordinate);
+    ctx.lineWidth = shape.lineWidth;
+    ctx.strokeStyle = shape.strokeStyle;
+    ctx.lineCap = shape.lineCap;
+    shape.arr.forEach(element => {
+      ctx.lineTo(element.xCoordinate, element.yCoordinate);
+      ctx.stroke();
+    });
+    ctx.closePath();
   }
 
   addInput = (x, y) => {
@@ -53,7 +53,7 @@ const commonModules = (() => {
         ctx.textAlign = "left";
         ctx.font = "14px sans-serif";
         ctx.fillText(textarea.value, textX, textY);
-        shapeLis.push(new Text(ctx, new Point(textX,textY), textarea.value));
+        shapeLis.push(new Text(ctx, new Point(textX, textY), textarea.value));
         document.body.removeChild(textarea);
       }
     };
@@ -63,7 +63,7 @@ const commonModules = (() => {
 
   setShape = (shapeName) => {
     commonModules.tool = "Shape";
-    commonModules.shape = new Shape(shapeName,ctx);
+    commonModules.shape = new Shape(shapeName, ctx);
   };
 
   colorId = "color1";
@@ -83,28 +83,28 @@ const commonModules = (() => {
   isMouseInShape = (shape) => {
 
     return (
-      (shape.shapeName == 'line' && shape.createGivenNameObj().isPointOnLine(commonModules.startX,commonModules.startY)) ||
-      shape.isPointOnShape(commonModules.startX,commonModules.startY)
+      (shape.shapeName == 'line' && shape.createGivenNameObj().isPointOnLine(commonModules.startX, commonModules.startY))
+      || shape.isPointOnShape(commonModules.startX, commonModules.startY)
     )
   };
 
   isMouseInText = (shape) => {
     return (
-      commonModules.startX > shape.location.xCoordinate &&
-      commonModules.startX < shape.location.xCoordinate + 400 &&
-      commonModules.startY > shape.location.yCoordinate &&
-      commonModules.startY < shape.location.yCoordinate + 14
+      commonModules.startX > shape.location.xCoordinate
+      && commonModules.startX < shape.location.xCoordinate + 400
+      && commonModules.startY > shape.location.yCoordinate
+      && commonModules.startY < shape.location.yCoordinate + 14
     )
   };
 
   rotation = (angle) => {
     ctx.clearRect(0, 0, canvasObj.canvas.width, canvasObj.canvas.height);
-    let index = commonModules.currentShapeIndex >= 0 ? commonModules.currentShapeIndex : shapeLis.length - 1;
 
+    let index = commonModules.currentShapeIndex >= 0 ? commonModules.currentShapeIndex : shapeLis.length - 1;
     let currentShape = shapeLis[index];
 
-    for(let i = 0; i < currentShape.positionArr.length; i++){
-      currentShape.positionArr[i].rotate(currentShape.center,angle);
+    for (let i = 0; i < currentShape.positionArr.length; i++) {
+      currentShape.positionArr[i].rotate(currentShape.center, angle);
     }
 
     draw();
